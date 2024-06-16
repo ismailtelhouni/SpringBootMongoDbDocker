b8e55066dee680d0ea9a3fb124e7a4afbe06860d
ismail_telhouni <118864341+ismailtelhouni@users.noreply.github.com>
2024-06-16 14:37:08 +0100

    add crud product and dockerize project , config file docker-compose.yml with mongodb

---
+++ src/main/java/com/example/springbootmongodockercompose/repo/ProductRepo.java
@@ -0,0 +1,8 @@
+package com.example.springbootmongodockercompose.repo;
+
+import com.example.springbootmongodockercompose.entity.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepo extends MongoRepository<Product, String> {
+
+}
